1f4854e151ccffd2d01a0f89f1fa0b92c0cf74d4
Farhan Mazario <farhanrio2805@gmail.com>
2020-12-02 22:49:06 +0700

    test

--- app/src/main/java/com/example/infofutsalvenueapp/ProfileActivity.java
+++ app/src/main/java/com/example/infofutsalvenueapp/ProfileActivity.java
@@ -3,12 +3,24 @@ package com.example.infofutsalvenueapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView icBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        icBack = findViewById(R.id.icBack);
+        icBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
